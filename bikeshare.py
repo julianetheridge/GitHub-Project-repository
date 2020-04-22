80347c4fad1aebff2bedfee9b9936afab7ce5491
Julian <julian.etheridge@outook.com>
2020-04-22 13:21:43 +0200

    function 'raw_data' renamed to 'display_data'

--- bikeshare.py
+++ bikeshare.py
@@ -51,7 +51,7 @@ def day_string_to_number(string):
         raise ValueError('Not a day')
 
 #user prompt to display the first five rows of the dataframe
-def raw_data(df):
+def display_data(df):
     see_data = ''
     n = 0
     m = 5
@@ -255,7 +255,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        raw_data(df)
+        display_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
